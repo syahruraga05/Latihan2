8f2addaa36a8088588421cd8707f01db9f6b0d0a
syahru <syahruragaramdhani@gmail.com>
2020-11-04 23:20:33 +0700

    Penjelasan Python

---
+++ lab2.py
@@ -0,0 +1,11 @@
+a=int(input("Masukkan Nilai A : "))
+b=int(input("Masukkan Nilai B : "))
+print("Variable A : ",a)
+print("Variable B : ",b)
+print("Hasil penggabungan {}&{}=%d".format(a,b) %(a+b))
+
+#Konversi nilai variable
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan {}+{}=%d".format(a,b) %(a+b))
+print("Hasil pembagian {}/{}=%d".format(a,b) %(a/b))
