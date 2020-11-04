b288ad951d3fea875b116f79442d3d561a1cbf03
syahru <syahruragaramdhani@gmail.com>
2020-11-04 21:53:44 +0700

    Penjelasan Python

---
+++ biodata.py
@@ -0,0 +1,20 @@
+print ("  ====================================")
+print ("       Latihan Biodata Syahru       ")
+print ("  ====================================")
+#variabel
+namalengkap   = input ("Masukan Nama Lengkap Anda: ")
+panggilan     = input ("Masukan Nama Panggilan: ")
+nim           = input ("Masukan Nim Anda: ")
+ttl           = input ("Masukan Tempat Tanggal Lahir Anda: ")
+umur          = input ("Masukan umur Anda: ")
+telepone      = input ("Masukan No Telpon Anda: ")
+alamat        = input ("Masukan Alamat Anda: ")
+
+#Menampilkan Inputan User
+print ("\nAssalamu'alaikum Wr.Wb.")
+print ("Let me introduce my self. My name is",namalengkap,\
+", but you can call me",panggilan,". My NIM is ",nim,\
+". I was born in",ttl,\
+"and I am ",umur,"years old",\
+"i am very glad if you want to invite my house in",alamat,\
+". So, don't forget to call me before with the number",telepone,".")
