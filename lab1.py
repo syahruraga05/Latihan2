13264980b5acce1a7c212eaf9e73509b8a51f70f
syahru <syahruragaramdhani@gmail.com>
2020-11-04 23:20:26 +0700

    Penjelasan Python

---
+++ lab1.py
@@ -0,0 +1,18 @@
+# penggunaan end
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('X')
+print('Y')
+print('Z')
+
+# pengggunaan separtor
+
+w, d, y, z = 10, 15, 20, 25
+print(w, d, y, z)
+print(w, d, y, z, sep='+')
+print(w, d, y, z, sep='x')
+print(w, d, y, z, sep=':')
+print(w, d, y, z, sep='-----')
+print(d, z, sep='=')
